0722b598844b8b7d1577a8b3355b90bd8316ef52
Andrew A <AndrewADev@gmail.com>
2018-07-14 12:59:39 -0400

    Enable Dashboard component
    Add the dashboard component to landing page
    
    Wire-up the feed service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FeedService } from './feed.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [FeedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
